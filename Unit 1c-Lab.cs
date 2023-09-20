25ad800303446bf7505f2ec1a5ac7e783581503e
JoshRockwood <143041293+JoshRockwood@users.noreply.github.com>
2023-09-19 21:24:56 -0600

    Updated Lab

--- Unit 1c-Lab.cs
+++ Unit 1c-Lab.cs
@@ -4,28 +4,22 @@ public class Program
 {
 	public static void Main()
 	{
+		int numberOfRows;
 		Console.WriteLine("Please enter a positive whole number.");
-		if (int.TryParse(Console.ReadLine(), out int numberOfRows) && numberOfRows > 0)
+		while (!int.TryParse(Console.ReadLine(), out numberOfRows) || numberOfRows <= 0)
 		{
-			Console.WriteLine();
-			for (int i = 1; i <= numberOfRows; i++)
-			{
-				for (int space = 0; space < numberOfRows - i; space++)
-				{
-					Console.Write(" ");
-				}
-
-				for (int j = 1; j <= i; j++)
-				{
-					Console.Write(i);
-				}
-
-				Console.WriteLine();
-			}
+			Console.WriteLine("Please enter a valid positive whole number.");
 		}
-				else
-				{
-					Console.WriteLine("Please enter a valid positive whole number.");
-				}
+		
+		Console.WriteLine();
+		for (int i = 1; i <= numberOfRows; i++)
+		{
+			for (int j = 1; j <= i; j++)
+			{
+				Console.Write(i);
+			}
+
+			Console.WriteLine();
+		}
 	}
-}
+} 
