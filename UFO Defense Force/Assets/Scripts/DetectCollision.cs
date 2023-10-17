fb45df7eea7f9eaed27784951497b2caef3a56bc
Josh Rockwood <joshrockwood@hotmail.com>
2023-10-17 17:26:05 -0600

    DetectCollision exclude player and enemy

--- UFO Defense Force/Assets/Scripts/DetectCollision.cs
+++ UFO Defense Force/Assets/Scripts/DetectCollision.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 
 public class DetectCollision : MonoBehaviour
 {
+    public ScoreManager scoreManager; // Store reference to score manager
+    public int scoreToGive;
+
+    void Start()
+    {
+        scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>(); // Find ScoreManager game object and reference ScoreManager script component
+    }
      void OnTriggerEnter(Collider other)
     {
-        // Destroys the other game object
-        Destroy(other.gameObject);
+        if (!other.CompareTag("Player") && !other.CompareTag("Enemy"))
+        {
+            // Increase the score
+            scoreManager.IncreaseScore(scoreToGive);
+            // Destroys the other game object
+            Destroy(other.gameObject);
+        }
         // Destroys game object
         Destroy(gameObject);
     }
