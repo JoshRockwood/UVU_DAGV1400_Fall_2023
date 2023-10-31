2b95ed91f00248f05e213996fb35765a1c7347f2
Josh Rockwood <joshrockwood@hotmail.com>
2023-10-31 14:59:38 -0600

    Created MonoEventsBehavior

---
+++ Scriptable Object/Assets/Scripts/MonoEventsBehavior.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MonoEventsBehavior : MonoBehaviour
+{
+    public UnityEvent awakeEvent, startEvent, disableEvent;
+
+    private void Awake()
+    {
+        awakeEvent.Invoke();
+    }
+    private void Start()
+    {
+        startEvent.Invoke();
+    }
+    private void OnDisable()
+    {
+        disableEvent.Invoke();
+    }
+
+}
