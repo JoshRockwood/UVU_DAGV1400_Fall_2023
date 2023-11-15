c217429058c02f29d26844fa45205be1d48ab09a
Josh Rockwood <joshrockwood@hotmail.com>
2023-11-14 23:22:17 -0700

    UpdateCoroutine

--- Color Match Game (2D)/Assets/Scripts/CoroutineBehaviour.cs
+++ Color Match Game (2D)/Assets/Scripts/CoroutineBehaviour.cs
@@ -4,24 +4,48 @@ using UnityEngine.Events;
 
 public class CoroutineBehaviour : MonoBehaviour
 {
-    public UnityEvent startEvent, repeatEvent, endEvent;
+    public bool canRun;
+    public UnityEvent startEvent, startCountEvent, repeatCountEvent, endCountEvent, repeatUntilFalseEvent;
     public IntData counterNum;
     public float seconds = 3.0f;
     private WaitForSeconds wfsObj;
     private WaitForFixedUpdate wffuObj;
-    IEnumerator Start()
+    
+    public void Start()
     {
         wfsObj = new WaitForSeconds(seconds);
         wffuObj = new WaitForFixedUpdate();
-       
-       startEvent.Invoke();
+        startEvent.Invoke();
+    }
+
+    public void StartCounting()
+    {
+        StartCoroutine(Counting());
+    }
+    private IEnumerator Counting()
+    {       
+       startCountEvent.Invoke();
        yield return wfsObj;
        while (counterNum.value > 0)
        {
-            repeatEvent.Invoke();
+            repeatCountEvent.Invoke();
             counterNum.value--;
             yield return wfsObj;
        }
-       endEvent.Invoke();
+       endCountEvent.Invoke();
+    }
+
+    public void StartRepeatUntilFalse()
+    {
+        canRun = true;
+        StartCoroutine(RepeatUntilFalse());
+    }
+    private IEnumerator RepeatUntilFalse()
+    {
+        while (canRun)
+        {
+            yield return wfsObj;
+            repeatUntilFalseEvent.Invoke();
+        }
     }
 }
